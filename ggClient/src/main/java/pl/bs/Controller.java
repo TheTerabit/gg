f059f5bad9b084b82bddd6e48305a127abf772cb
TheTerabit <terrabitroman@gmail.com>
2020-01-08 14:58:39 +0100

    all views added

--- ggClient/src/main/java/pl/bs/Controller.java
+++ ggClient/src/main/java/pl/bs/Controller.java
@@ -1,4 +1,22 @@
 package pl.bs;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Controller {
+    public void logIn(ActionEvent actionEvent) {
+
+        String msg = "some text";
+        try {
+            Socket clientSocket = new Socket("192.168.65.130", 1234,true);
+            OutputStream os = clientSocket.getOutputStream();
+            os.write(msg.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("hi");
+    }
 }
--- ggClient/src/main/java/pl/bs/Main.java
+++ ggClient/src/main/java/pl/bs/Main.java
@@ -1,15 +1,21 @@
 package pl.bs;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
+
+    Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
         primaryStage.setTitle("Welcome to GG");
         primaryStage.setScene(new Scene(root));
@@ -17,6 +23,9 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public void logIn(ActionEvent actionEvent) {
+        stage.close();
+    }
 
     public static void main(String[] args) {
         launch(args);
